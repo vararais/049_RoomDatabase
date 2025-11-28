58be0dfe0f22e56e4642f2b7fa939c7b4223cc12
vararais <varadigmaisnann@gmail.com>
2025-11-28 21:33:30 +0700

    Menambah code dalam destinasi home

--- app/src/main/java/com/example/roomdatabase/view/route/DestinasiHome.kt
+++ app/src/main/java/com/example/roomdatabase/view/route/DestinasiHome.kt
@@ -1,4 +1,9 @@
 package com.example.roomdatabase.view.route
 
-object DestinasiHome {
+
+import com.example.roomdatabase.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
 }
