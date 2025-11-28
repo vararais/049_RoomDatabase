3d9db438919dc0c76b6fcb585b0c1aa45071baea
vararais <varadigmaisnann@gmail.com>
2025-11-28 15:15:55 +0700

    Membuat file siswa dan menambah fungsi entitas di dalam file siswa

---
+++ app/src/main/java/com/example/roomdatabase/room/Siswa.kt
@@ -0,0 +1,14 @@
+package com.example.roomdatabase.room
+
+import androidx.compose.ui.input.pointer.PointerId
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tblSiswa")
+data class Siswa(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val nama: String,
+    val alamat: String,
+    val telpon: String
+)
