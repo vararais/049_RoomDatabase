c4165f0d8d25da1ed0a7540d2e46c7d837e10a55
vararais <varadigmaisnann@gmail.com>
2025-11-28 15:41:39 +0700

    Membuat public interface Repositori Siswa

--- app/src/main/java/com/example/roomdatabase/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/roomdatabase/repositori/RepositoriSiswa.kt
@@ -11,4 +11,7 @@ interface RepositoriSiswa {
 
 class OfflineRepositoriSiswa(
     private val siswaDao: SiswaDao
-):
+): RepositoriSiswa {
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+}
