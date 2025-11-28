a99f40a99d2ba5bef8499f64a634207314c82761
vararais <varadigmaisnann@gmail.com>
2025-11-28 15:38:30 +0700

    Menambah interface dan fungsi get dan insert

--- app/src/main/java/com/example/roomdatabase/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/roomdatabase/repositori/RepositoriSiswa.kt
@@ -1,2 +1,11 @@
 package com.example.roomdatabase.repositori
 
+import com.example.roomdatabase.room.Siswa
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa {
+    fun getAllSiswaStream(): Flow<List<Siswa>>
+    suspend fun insertSiswa(siswa: Siswa)
+
+
+}
