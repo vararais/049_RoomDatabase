721e0bf4135967aca399de493134117ecb6db57e
vararais <varadigmaisnann@gmail.com>
2025-11-28 15:28:41 +0700

    Menambah abstract class dan fungsinya

--- app/src/main/java/com/example/roomdatabase/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/roomdatabase/room/DatabaseSiswa.kt
@@ -1,6 +1,11 @@
 package com.example.roomdatabase.room
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 
 @Database(entities = [Siswa::class], version = 1, exportSchema = false)
-data class DatabaseSiswa()
+abstract class DatabaseSiswa : RoomDatabase(){
+    abstract fun SiswaDao() : SiswaDao
+
+
+}
