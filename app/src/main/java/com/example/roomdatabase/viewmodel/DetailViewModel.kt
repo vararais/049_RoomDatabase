d01ffa6040793fde75bb71e9e6d93e934504b5cd
vararais <varadigmaisnann@gmail.com>
2025-12-05 15:13:27 +0700

    Menambah import

--- app/src/main/java/com/example/roomdatabase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/viewmodel/DetailViewModel.kt
@@ -2,10 +2,14 @@ package com.example.roomdatabase.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.roomdatabase.repositori.RepositoriSiswa
 import com.example.roomdatabase.view.route.DestinasiDetailSiswa
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 
 class DetailViewModel (
     savedStateHandle: SavedStateHandle,
