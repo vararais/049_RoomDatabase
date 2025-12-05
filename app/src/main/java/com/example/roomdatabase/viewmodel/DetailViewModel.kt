516cafcfbc91c796a5771775547b24d0e8b5b227
vararais <varadigmaisnann@gmail.com>
2025-12-05 15:12:32 +0700

    Menambah companion object

--- app/src/main/java/com/example/roomdatabase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/viewmodel/DetailViewModel.kt
@@ -27,4 +27,11 @@ class DetailViewModel (
         repositoriSiswa.deleteSiswa(uiDetailState.value.detailSiswa.toSiswa())
     }
 
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+}
+
+
+
 
