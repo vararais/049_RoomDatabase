7eeee7eaa52cea712298eb454b7e9bb58f99b855
vararais <varadigmaisnann@gmail.com>
2025-12-05 14:58:30 +0700

    Membuat fungsi override jeda update siswa

--- app/src/main/java/com/example/roomdatabase/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/roomdatabase/repositori/RepositoriSiswa.kt
@@ -23,5 +23,5 @@ class OfflineRepositorisiswa(
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
     override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
     override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
-
+    override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
 }
