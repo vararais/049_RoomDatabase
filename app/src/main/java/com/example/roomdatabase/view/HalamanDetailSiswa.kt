4031db490b8679e102a90ba122e89fbf80c4b8ce
vararais <varadigmaisnann@gmail.com>
2025-12-05 15:20:54 +0700

    Menambah Detail data siswa, Outlined button dan validasi konfirmasi delete

--- app/src/main/java/com/example/roomdatabase/view/HalamanDetailSiswa.kt
+++ app/src/main/java/com/example/roomdatabase/view/HalamanDetailSiswa.kt
@@ -66,3 +66,27 @@ private fun BodyDetailDataSiswa(
         verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
     ) {
         var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+
+        DetailDataSiswa(
+            siswa = detailSiswaUiState.detailSiswa.toSiswa(),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedButton(
+            onClick = { deleteConfirmationRequired = true },
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.delete))
+        }
+        if (deleteConfirmationRequired) {
+            DeleteConfirmationDialog(
+                onDeleteConfirm = {
+                    deleteConfirmationRequired = false
+                    onDelete()
+                },
+                onDeleteCancel = { deleteConfirmationRequired = false },
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+            )
+        }
+    }
+}
